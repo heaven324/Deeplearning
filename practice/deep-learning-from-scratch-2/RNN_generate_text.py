f48a325a00ef52bfba39334b1e410aafc4da05d9
heaven <wjdtjdgh2005@gmail.com>
2019-12-12 20:41:32 +0900

    practice uploading 	add generate text code
     Changes to be committed:
    	new file:   practice/deep-learning-from-scratch-2/RNN_RNNLM_gen.py
    	new file:   practice/deep-learning-from-scratch-2/RNN_generate_better_text.py
    	new file:   practice/deep-learning-from-scratch-2/RNN_generate_text.py

---
+++ practice/deep-learning-from-scratch-2/RNN_generate_text.py
@@ -0,0 +1,21 @@
+from RNN_RNNLM_gen import RnnlmGen
+import ptb
+
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+corpus_size = len(corpus)
+
+model = RnnlmGen()
+model.load_params('Rnnlm.pkl')
+
+# set start text and skip text
+start_word = 'you'
+start_id = word_to_id[start_word]
+skip_words = ['N', '<unk>', '$']
+skip_ids = [word_to_id[w] for w in skip_words]
+
+# Generate sentence
+word_ids = model.generate(start_id, skip_ids)
+txt = ' '.join([id_to_word[i] for i in word_ids])
+txt = txt.replace(' <eos>', '.\n')
+print(txt)
